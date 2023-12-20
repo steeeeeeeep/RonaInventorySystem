e8ecb4d5d250bdfd184d20d8cadbe0108ca0e04f
steeeeeeeep <stephenedave.dorado@msugensan.edu.ph>
2023-12-20 21:32:45 +0800

    create invistor page

---
+++ Pages/GetInvistors.razor.cs
@@ -0,0 +1,31 @@
+﻿namespace BikeSparesInventorySystem.Pages;
+
+public partial class GetInvistors 
+{
+    public const string route = "/invistors";
+    private readonly bool Dense = true;
+    private readonly bool Fixed_header = true;
+    private readonly bool Fixed_footer = true;
+    private readonly bool Hover = true;
+    private bool ReadOnly = false;
+    private readonly bool CanCancelEdit = true;
+    private readonly bool BlockSwitch = true;
+    private string SearchString;
+    private Spare SelectedItem;
+    private Spare ElementBeforeEdit;
+
+    private readonly TableApplyButtonPosition ApplyButtonPosition = TableApplyButtonPosition.End;
+    private readonly TableEditButtonPosition EditButtonPosition = TableEditButtonPosition.End;
+    private readonly TableEditTrigger EditTrigger = TableEditTrigger.RowClick;
+    private IEnumerable<Invistor> Elements;
+    private readonly Dictionary<Guid, bool> SpareDescTracks = new();
+
+
+    [CascadingParameter]
+    private Action<string> SetAppBarTitle { get; set; }
+    protected sealed override void OnInitialized()
+    {
+        SetAppBarTitle.Invoke("Invistors");
+        Elements = InvistorRepository.GetAll();
+    }
+}
