f0d0ed1177f10ede8cc1a8ae5604e44d5ea6d54e
steeeeeeeep <stephenedave.dorado@msugensan.edu.ph>
2023-12-20 21:33:37 +0800

    Pages/Inventory: uncomment code

--- Pages/Inventory.razor.cs
+++ Pages/Inventory.razor.cs
@@ -1,127 +1,156 @@
-﻿//namespace BikeSparesInventorySystem.Pages;
+﻿using Color = MudBlazor.Color;
 
-//public partial class Inventory
-//{
-//    public const string Route = "/inventory";
+namespace BikeSparesInventorySystem.Pages;
 
-//    private readonly bool Dense = true;
-//    private readonly bool Fixed_header = true;
-//    private readonly bool Fixed_footer = true;
-//    private readonly bool Hover = true;
-//    private bool ReadOnly = false;
-//    private readonly bool CanCancelEdit = true;
-//    private readonly bool BlockSwitch = true;
-//    private string SearchString;
-//    private Spare SelectedItem;
-//    private Spare ElementBeforeEdit;
-    
-//    private readonly TableApplyButtonPosition ApplyButtonPosition = TableApplyButtonPosition.End;
-//    private readonly TableEditButtonPosition EditButtonPosition = TableEditButtonPosition.End;
-//    private readonly TableEditTrigger EditTrigger = TableEditTrigger.RowClick;
-//    private IEnumerable<Spare> Elements;
-//    private readonly Dictionary<Guid, bool> SpareDescTracks = new();
+public partial class Inventory
+{
+    public const string Route = "/inventory";
 
-//    [CascadingParameter]
-//    private Action<string> SetAppBarTitle { get; set; }
+    private readonly bool Dense = true;
+    private readonly bool Fixed_header = true;
+    private readonly bool Fixed_footer = true;
+    private readonly bool Hover = true;
+    private bool ReadOnly = false;
+    private readonly bool CanCancelEdit = true;
+    private readonly bool BlockSwitch = true;
+    private string SearchString;
+    private Spare SelectedItem;
+    private Spare ElementBeforeEdit;
 
-//    protected sealed override void OnInitialized()
-//    {
+    private readonly TableApplyButtonPosition ApplyButtonPosition = TableApplyButtonPosition.End;
+    private readonly TableEditButtonPosition EditButtonPosition = TableEditButtonPosition.End;
+    private readonly TableEditTrigger EditTrigger = TableEditTrigger.RowClick;
+    private IEnumerable<Spare> Elements;
+    private IEnumerable<Category> Categories;
+    private readonly Dictionary<Guid, bool> SpareDescTracks = new();
+    private Color Colors { get; set; }
 
-//        SetAppBarTitle.Invoke("Manage Products");
-//        Elements = SpareRepository.GetAll();
-//        if (!AuthService.IsUserAdmin())
-//        {
-//            ReadOnly = true;
-//        }
-//        foreach (Spare s in Elements)
-//        {
-//            SpareDescTracks.Add(s.Id, false);
-//        }
+    [CascadingParameter]
+    private Action<string> SetAppBarTitle { get; set; }
+    [Inject] public NavigationManager Navigation { get; set; } = default!;
 
-//    }
+    protected sealed override void OnInitialized()
+    {
+        Categories = CategoryRepository.GetAll();
+        SetAppBarTitle.Invoke("Manage Products");
+        Elements = SpareRepository.GetAll();
 
-//    protected async Task Update()
-//    {
-//        await SpareRepository.FlushAsync();
-//        Snackbar.Add("Successfuly Updated!", Severity.Success);
-//    }
+        if (!AuthService.IsUserAdmin())
+        {
+            ReadOnly = true;
+        }
+        foreach (Spare s in Elements)
+        {
+            SpareDescTracks.Add(s.Id, false);
+        }
 
-//    protected string GetCategoryNameById(Guid Id)
-//    {
-//        return CategoryRepository.Get(x => x.Id, Id).Name;
-//    }
+        if(Categories.Count() == 0)
+        {
+            Colors = Color.Default;
+        }
+    }
 
+    protected async Task Update()
+    {
+        await SpareRepository.FlushAsync();
+        Snackbar.Add("Successfuly Updated!", Severity.Success);
+    }
 
-//    private void BackupItem(object element)
-//    {
-//        ElementBeforeEdit = ((Spare)element).Clone() as Spare;
-//    }
+    protected string GetCategoryNameById(Guid Id)
+    {
+        return CategoryRepository.Get(x => x.Id, Id).Name;
+    }
 
-//    private void ResetItemToOriginalValues(object element)
-//    {
-//        ((Spare)element).Name = ElementBeforeEdit.Name;
-//        ((Spare)element).Description = ElementBeforeEdit.Description;
-//        ((Spare)element).CategoryId = ElementBeforeEdit.CategoryId;
-//        ((Spare)element).Price = ElementBeforeEdit.Price;
-//        ((Spare)element).AvailableQuantity = ElementBeforeEdit.AvailableQuantity;
-//    }
+    protected void ShowCategoryPage()
+    {
+        Navigation.NavigateTo("/category");
+    }
 
-//    private bool FilterFunc(Spare element)
-//    {
-//        return string.IsNullOrWhiteSpace(SearchString)
-//               || element.Id.ToString().Contains(SearchString, StringComparison.OrdinalIgnoreCase)
-//               || element.Name.Contains(SearchString, StringComparison.OrdinalIgnoreCase)
-//               || element.Description.Contains(SearchString, StringComparison.OrdinalIgnoreCase)
-//               || element.Price.ToString().Contains(SearchString, StringComparison.OrdinalIgnoreCase)
-//               || element.AvailableQuantity.ToString().Contains(SearchString, StringComparison.OrdinalIgnoreCase);
-//    }
+    private void BackupItem(object element)
+    {
+        ElementBeforeEdit = ((Spare)element).Clone() as Spare;
+    }
 
-//    private void ShowBtnPress(Guid id)
-//    {
-//        SpareDescTracks[id] = !SpareDescTracks[id];
-//    }
+    private void ResetItemToOriginalValues(object element)
+    {
+        ((Spare)element).Name = ElementBeforeEdit.Name;
+        ((Spare)element).Description = ElementBeforeEdit.Description;
+        ((Spare)element).CategoryId = ElementBeforeEdit.CategoryId;
+        ((Spare)element).Price = ElementBeforeEdit.Price;
+        ((Spare)element).AvailableQuantity = ElementBeforeEdit.AvailableQuantity;
+    }
 
-//    private bool GetShow(Guid id)
-//    {
-//        return SpareDescTracks.ContainsKey(id) ? SpareDescTracks[id] : (SpareDescTracks[id] = false);
-//    }
+    private bool FilterFunc(Spare element)
+    {
+        return string.IsNullOrWhiteSpace(SearchString)
+               || element.Id.ToString().Contains(SearchString, StringComparison.OrdinalIgnoreCase)
+               || element.Name.Contains(SearchString, StringComparison.OrdinalIgnoreCase)
+               || element.Description.Contains(SearchString, StringComparison.OrdinalIgnoreCase)
+               || element.Price.ToString().Contains(SearchString, StringComparison.OrdinalIgnoreCase)
+               || element.AvailableQuantity.ToString().Contains(SearchString, StringComparison.OrdinalIgnoreCase);
+    }
 
-//    private string GetLastTakenOut(Guid id)
-//    {
-//        List<ActivityLog> log = ActivityLogRepository.GetAll().Where(x => x.SpareID == id && x.Action == StockAction.Deduct && x.ApprovalStatus == ApprovalStatus.Approve).ToList();
-//        return log.Count == 0 ? "N/A" : log.Max(x => x.ApprovalStatusOn).ToString();
-//    }
+    private void ShowBtnPress(Guid id)
+    {
+        SpareDescTracks[id] = !SpareDescTracks[id];
+    }
 
-//    private async Task AddDialog()
-//    {
-//        DialogParameters parameters = new()
-//        {
-//            { "ChangeParentState", new Action(StateHasChanged) }
-//        };
-//        await DialogService.ShowAsync<AddSpareDialog>("Add Product", parameters);
-//    }
+    private bool GetShow(Guid id)
+    {
+        return SpareDescTracks.ContainsKey(id) ? SpareDescTracks[id] : (SpareDescTracks[id] = false);
+    }
 
-//    private async Task ActOnStock(Spare spare, StockAction action)
-//    {
-//        if (action == StockAction.Deduct)
-//        {
-//            if (!ApproveButton.ValidateWeekAndTime(Snackbar))
-//            {
-//                return;
-//            }
+    private string GetLastTakenOut(Guid id)
+    {
+        List<ActivityLog> log = ActivityLogRepository.GetAll().Where(x => x.SpareID == id && x.Action == StockAction.Deduct && x.ApprovalStatus == ApprovalStatus.Approve).ToList();
+        return log.Count == 0 ? "N/A" : log.Max(x => x.ApprovalStatusOn).ToString();
+    }
 
-//            if (spare.AvailableQuantity == 0)
-//            {
-//                Snackbar.Add("Out of Stock!", Severity.Error);
-//                return;
-//            }
-//        }
-//        DialogParameters parameters = new()
-//        {
-//            { "StockAction", action },
-//            { "Spare",  spare},
-//            { "ChangeParentState", new Action(StateHasChanged) }
-//        };
-//        await DialogService.ShowAsync<StockActionDialog>($"{Enum.GetName(action)} Stock", parameters);
-//    }
-//}
+    private async Task AddDialog()
+    {
+        if(Categories.Any())
+        {
+            DialogParameters parameters = new()
+            {
+                { "ChangeParentState", new Action(StateHasChanged) }
+            };
+            await DialogService.ShowAsync<AddSpareDialog>("Add Product", parameters);
+        }
+        else
+        {
+            Snackbar.Add("Create product category first! Click here!", Severity.Error, (option) =>
+            {
+                option.CloseAfterNavigation = true;
+                option.Onclick = onclick =>
+                {
+                    ShowCategoryPage();
+                    return Task.CompletedTask;
+                };
+            });
+        }
+    }
+
+    private async Task ActOnStock(Spare spare, StockAction action)
+    {
+        if (action == StockAction.Deduct)
+        {
+            if (!ApproveButton.ValidateWeekAndTime(Snackbar))
+            {
+                return;
+            }
+
+            if (spare.AvailableQuantity == 0)
+            {
+                Snackbar.Add("Out of Stock!", Severity.Error);
+                return;
+            }
+        }
+        DialogParameters parameters = new()
+        {
+            { "StockAction", action },
+            { "Spare",  spare},
+            { "ChangeParentState", new Action(StateHasChanged) }
+        };
+        await DialogService.ShowAsync<StockActionDialog>($"{Enum.GetName(action)} Stock", parameters);
+    }
+}
