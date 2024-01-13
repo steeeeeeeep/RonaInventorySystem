ebb5f34a29277d1809f1fd11036192614be00381
steeeeeeeep <stephenedave.dorado@msugensan.edu.ph>
2024-01-13 17:47:10 +0800

    add on debug method

--- Data/Repositories/Repository.Extension.cs
+++ Data/Repositories/Repository.Extension.cs
@@ -5,5 +5,13 @@ internal static class Repository
     public static bool HasUserName(this Repository<User> userRepository, string userName)
     {
         return userRepository.Contains(x => x.UserName, userName);
-    }
+	}
+
+	public static void OnDebugConsoleWriteUserNames(this Repository<User> userRepository)
+	{
+		foreach (User i in userRepository.GetAll())
+		{
+			System.Diagnostics.Debug.WriteLine($"{{ Username = {i.UserName} , InitialPassword = {i.HasInitialPassword} , Role = {i.Role}}}");
+		}
+	}
 }
