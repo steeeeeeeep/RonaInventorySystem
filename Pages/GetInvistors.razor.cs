da48a3e9073e88faf8df12b636b7f51068a5cc5b
steeeeeeeep <stephenedave.dorado@msugensan.edu.ph>
2024-01-13 17:53:50 +0800

    show add dialog

--- Pages/GetInvistors.razor.cs
+++ Pages/GetInvistors.razor.cs
@@ -7,22 +7,21 @@ public partial class GetInvistors
     private readonly bool Fixed_header = true;
     private readonly bool Fixed_footer = true;
     private readonly bool Hover = true;
-    private bool ReadOnly = false;
+    private readonly bool ReadOnly = false;
     private readonly bool CanCancelEdit = true;
-    private readonly bool BlockSwitch = true;
     private string SearchString;
-    private Spare SelectedItem;
-    private Spare ElementBeforeEdit;
 
-    private readonly TableApplyButtonPosition ApplyButtonPosition = TableApplyButtonPosition.End;
-    private readonly TableEditButtonPosition EditButtonPosition = TableEditButtonPosition.End;
-    private readonly TableEditTrigger EditTrigger = TableEditTrigger.RowClick;
     private IEnumerable<Invistor> Elements;
-    private readonly Dictionary<Guid, bool> SpareDescTracks = new();
 
 
     [CascadingParameter]
     private Action<string> SetAppBarTitle { get; set; }
+
+    [Inject]
+    public IDialogService Dialog { get; set; } = default!;
+
+    public DialogOptions _options = new() { MaxWidth = MaxWidth.Large };
+
     protected sealed override void OnInitialized()
     {
         SetAppBarTitle.Invoke("Invistors");
@@ -31,6 +30,11 @@ public partial class GetInvistors
 
     protected async Task AddDialog()
     {
+        DialogParameters parameters = new()
+        {
+            { "ChangeParentState", new Action(StateHasChanged) }
+        };
 
+        await Dialog.ShowAsync<AddInvistor>("Add Invistor", parameters, _options);
     }
 }
