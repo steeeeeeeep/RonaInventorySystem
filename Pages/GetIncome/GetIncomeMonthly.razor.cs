2c1f390c3bbda40798afbf8b54560012da72f896
steeeeeeeep <stephenedave.dorado@msugensan.edu.ph>
2024-02-17 19:04:30 +0800

    refactor get income monthly

--- Pages/GetIncome/GetIncomeMonthly.razor.cs
+++ Pages/GetIncome/GetIncomeMonthly.razor.cs
@@ -1,67 +1,135 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿namespace BikeSparesInventorySystem.Pages.GetIncome;
 
-//namespace BikeSparesInventorySystem.Pages.GetIncome;
+public partial class GetIncomeMonthly
+{
+    private readonly bool Dense = true;
+    private readonly bool Fixed_header = true;
+    private readonly bool Fixed_footer = true;
+    private readonly bool Hover = true;
+    private bool ReadOnly = false;
+    private readonly bool CanCancelEdit = true;
+    private readonly bool BlockSwitch = true;
+    private Sales SelectedItem;
+    private Sales ElementBeforeEdit;
+    private readonly TableApplyButtonPosition ApplyButtonPosition = TableApplyButtonPosition.End;
+    private readonly TableEditButtonPosition EditButtonPosition = TableEditButtonPosition.End;
+    private readonly TableEditTrigger EditTrigger = TableEditTrigger.EditButton;
+    protected int count;
+    private string SearchString;
+    private IEnumerable<MonthlySales> _MonthlySales;
+    private IEnumerable<Sales> _Sales;
+    private Contributions _GetActiveContributions;
 
-//public partial class GetIncomeMonthly
-//{
-//    public const string Route = "/dailyIncome";
+    [CascadingParameter]
+    private Action<string> SetAppBarTitle { get; set; }
+    [Inject]
+    public ISnackbar Snackbar { get; set; } = default!;
 
-//    private readonly bool Dense = true;
-//    private readonly bool Fixed_header = true;
-//    private readonly bool Fixed_footer = true;
-//    private readonly bool Hover = true;
-//    private bool ReadOnly = false;
-//    private readonly bool CanCancelEdit = true;
-//    private readonly bool BlockSwitch = true;
-//    private Sales SelectedItem;
-//    private Sales ElementBeforeEdit;
-//    private readonly TableApplyButtonPosition ApplyButtonPosition = TableApplyButtonPosition.End;
-//    private readonly TableEditButtonPosition EditButtonPosition = TableEditButtonPosition.End;
-//    private readonly TableEditTrigger EditTrigger = TableEditTrigger.EditButton;
-//    protected Guid categoryId;
-//    protected int count;
-//    private string SearchString;
-//    private IEnumerable<Sales> _Sales;
+    protected sealed override void OnInitialized()
+    {
+        SetAppBarTitle.Invoke("Manage Sales");
+        _MonthlySales = MonthlySalesRepository.GetAll();
+        _GetActiveContributions = ContributionRepository.Get(s => s.Status, Status.Active);
+    }
 
-//    [CascadingParameter]
-//    private Action<string> SetAppBarTitle { get; set; }
+    private bool FilterFunc(MonthlySales sales)
+    {
+        return string.IsNullOrWhiteSpace(SearchString)
+        || sales.Id.ToString().Contains(SearchString, StringComparison.OrdinalIgnoreCase)
+        || sales.Month.ToString("MM/dd/yyyy").Contains(SearchString, StringComparison.OrdinalIgnoreCase)
+        || sales.Purchases.ToString().Contains(SearchString, StringComparison.OrdinalIgnoreCase)
+        || sales.GrossSale.ToString().Contains(SearchString, StringComparison.OrdinalIgnoreCase)
+        || sales.Profit.ToString().Contains(SearchString, StringComparison.OrdinalIgnoreCase);
+    }
 
-//    public Guid _purchaseId { get; set; }
-//    public Guid _expenseId { get; set; }
+    private void FilterByMonth(string a)
+    {
+        ICollection<MonthlySales> _sales = MonthlySalesRepository.GetAll();
+        if (string.IsNullOrEmpty(a))
+        {
+            _MonthlySales = _sales;
+            return;
+        }
+        string[] date = a.Split('-');
+        //_MonthlySales = _sales.Where(d => d.Month.Year == int.Parse(date[0]) && d.Month.Month == int.Parse(date[1])).ToList();
+    }
 
-//    protected sealed override void OnInitialized()
-//    {
-//        SetAppBarTitle.Invoke("Manage Sales");
-//        _Sales = SalesRepository.GetAll().OrderBy(s => s.DailyDate);
-//    }
+    private async Task SyncSales()
+    {
+        _Sales = SalesRepository.GetAll().ToList();
+        var getData = 
+            _Sales.GroupBy(d => d.DailyDate.Date)
+            .Select(group => new
+            {
+                Id = group.Select(x => x.Id).FirstOrDefault(),
+                Date = group.Key,
+                GrossSale = group.Sum(s => s.GrossSale),
+                MonthlySale = group.Sum(s => s.DailyNetIncome),
+                Expense = group.Sum(s => s.Expenses),
+                _Purchase = group.Sum(s => s.Purchases),
+                Tithes = group.Sum(s => s.Tithes),
+                Car = group.Sum(s => s.Car),
+                Charity = group.Sum(s => s.Charity),
+                OtherContribution = group.Sum(s => s.OtherContribution),
+                Profit = group.Sum(s => s.Profit)
+            })
+            .OrderBy(o => o.Date);
 
-//    private bool FilterFunc(Sales sales)
-//    {
-//        return string.IsNullOrWhiteSpace(SearchString)
-//        || sales.Id.ToString().Contains(SearchString, StringComparison.OrdinalIgnoreCase)
-//        || sales.DailyDate.ToString("MM/dd/yyyy").Contains(SearchString, StringComparison.OrdinalIgnoreCase)
-//        || sales.PurchaseDate.ToString("MM/dd/yyyy").Contains(SearchString, StringComparison.OrdinalIgnoreCase)
-//        || sales.DailyNetIncome.ToString().Contains(SearchString, StringComparison.OrdinalIgnoreCase)
-//        || sales.Purchases.ToString().Contains(SearchString, StringComparison.OrdinalIgnoreCase)
-//        || sales.GrossSale.ToString().Contains(SearchString, StringComparison.OrdinalIgnoreCase)
-//        || sales.Profit.ToString().Contains(SearchString, StringComparison.OrdinalIgnoreCase);
-//    }
+        foreach(var sale in getData)
+        {
+            if(!_MonthlySales.Any(a => a.Month == sale.Date.Month && a.Year == sale.Date.Year) 
+                && !_MonthlySales.Any(a => a.Id == sale.Id))
+            {
+                MonthlySales newMonthlySales = new()
+                {
+                    GrossSale = sale.GrossSale,
+                    MonthlyNetIncome = sale.MonthlySale,
+                    Tithes = sale.Tithes,
+                    Car = sale.Car,
+                    Charity = sale.Charity,
+                    OtherContribution = sale.OtherContribution,
+                    Profit = sale.Profit,
+                    Expenses = sale.Expense,
+                    Purchases = sale._Purchase,
+                    Month = sale.Date.Month,
+                    Year = sale.Date.Year
+                };
 
-//    private void FilterByMonth(string a)
-//    {
-//        ICollection<Sales> _sales = SalesRepository.GetAll();
-//        if (string.IsNullOrEmpty(a))
-//        {
-//            _Sales = _sales;
-//            return;
-//        }
-//        string[] date = a.Split('-');
-//        _Sales = _sales.Where(d => d.DailyDate.Year == int.Parse(date[0]) && d.DailyDate.Month == int.Parse(date[1])).ToList();
-//    }
-//}
+                MonthlySalesRepository.Add(newMonthlySales);
+                await MonthlySalesRepository.FlushAsync();
+                Snackbar.Add("Sales is updated!", Severity.Info);
+            }
+            else if(_MonthlySales.Any(a => a.Month == sale.Date.Month && a.Year == sale.Date.Year)
+                && !_MonthlySales.Any(a => a.Id == sale.Id))
+            {
+                var existing = MonthlySalesRepository.Get(g => g.Month, sale.Date.Month);
+                if (existing != null && existing.Year == sale.Date.Year)
+                {
+                    existing.GrossSale = sale.GrossSale;
+                    existing.Tithes = sale.Tithes;
+                    existing.Car = sale.Car;
+                    existing.Charity = sale.Charity;
+                    existing.OtherContribution = sale.OtherContribution;
+                    existing.Profit = sale.Profit;
+                    existing.Expenses = sale.Expense;
+                    existing.Purchases = sale._Purchase;
+                    existing.Month = sale.Date.Month;
+                    existing.Year = sale.Date.Year;
+                }
+                else
+                {
+                    Snackbar.Add("Cannot get monthly income!");
+                }
 
-//}
+                await MonthlySalesRepository.FlushAsync();
+                Snackbar.Add("Sync complete", Severity.Info);
+            }
+            else
+            {
+                Snackbar.Add("Sales is up to date!", Severity.Warning);
+            }
+        }
+
+    }
+
+}
