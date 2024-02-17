cf81af8bcc87bc6f7dd586ecc2be12c6b5960c40
steeeeeeeep <stephenedave.dorado@msugensan.edu.ph>
2024-02-17 19:00:50 +0800

    create date model for contributions

---
+++ Data/Models/Contributions.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace BikeSparesInventorySystem.Data.Models;
+
+public class Contributions : IModel
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public double ? Tithes { get; set; }
+    public double ? CarExpense { get; set; }
+    public double ? Charity { get; set; }
+    public double ? Others { get; set; }
+    public Status Status { get; set; }
+
+    public object Clone()
+    {
+        return new Contributions
+        {
+            Id = Id,
+            Tithes = Tithes,
+            CarExpense = CarExpense,
+            Charity = Charity,
+            Others = Others,
+            Status = Status
+        };
+    }
+
+    public override string ToString()
+    {
+        return JsonSerializer.Serialize(this);
+    }
+}
