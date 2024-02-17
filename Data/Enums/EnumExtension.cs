8b21f08b58fccbb86851b45b78005c49693f04dd
steeeeeeeep <stephenedave.dorado@msugensan.edu.ph>
2024-02-17 19:00:26 +0800

    create enum extension

---
+++ Data/Enums/EnumExtension.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using System.Runtime.Serialization;
+
+namespace BikeSparesInventorySystem.Data.Enums;
+
+public static class EnumExtensions
+{
+    public static string GetDisplayName(this Enum value)
+    {
+        var fieldInfo = value.GetType().GetField(value.ToString());
+
+        if (fieldInfo != null)
+        {
+            var displayAttribute = fieldInfo.GetCustomAttribute<DisplayAttribute>();
+            if (displayAttribute != null)
+            {
+                return displayAttribute.Name;
+            }
+        }
+
+        return value.ToString();
+    }
+
+    public static string GetEnumValue(this Enum value)
+    {
+        var fieldInfo = value.GetType().GetField(value.ToString());
+
+        if (fieldInfo != null)
+        {
+            var enumMemberAttribute = fieldInfo.GetCustomAttribute<EnumMemberAttribute>();
+            if (enumMemberAttribute != null)
+            {
+                return enumMemberAttribute.Value;
+            }
+        }
+
+        return value.ToString();
+    }
+}
