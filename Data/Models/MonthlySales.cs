e1a2d1b1b6a40f17c30ab03fb28afd2921f88bbe
steeeeeeeep <stephenedave.dorado@msugensan.edu.ph>
2024-02-17 19:01:08 +0800

    create data model for monthly sales

---
+++ Data/Models/MonthlySales.cs
@@ -0,0 +1,46 @@
+﻿
+namespace BikeSparesInventorySystem.Data.Models;
+
+public class MonthlySales : IModel
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public decimal GrossSale { get; set; }
+    public decimal MonthlyNetIncome { get; set; }
+    public decimal Tithes { get; set; }
+    public decimal Charity { get; set; }
+    public decimal OtherContribution { get; set; }
+    public decimal Expenses { get; set; }
+    public decimal Car { get; set; }
+    public decimal Profit { get; set; }
+    public decimal SalesEntry { get; set; }
+    public decimal Purchases { get; set; }
+    public int Month { get; set; }
+    public int Year { get; set; }
+    public DateTime? CreatedAt { get; set; }
+
+    public object Clone()
+    {
+        return new MonthlySales
+        {
+            Id = Id,
+            GrossSale = GrossSale,
+            MonthlyNetIncome = MonthlyNetIncome,
+            Tithes = Tithes,
+            Charity = Charity,
+            OtherContribution = OtherContribution,
+            Expenses = Expenses,
+            Car = Car,
+            Profit = Profit,
+            SalesEntry = SalesEntry,
+            Purchases = Purchases,
+            Month = Month,
+            Year = Year,
+            CreatedAt = CreatedAt
+        };
+    }
+
+    public override string ToString()
+    {
+        return JsonSerializer.Serialize(this);
+    }
+}
