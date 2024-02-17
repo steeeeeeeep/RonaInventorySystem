6fd4c995a61405110b418750bcba1b73ecdc18a1
steeeeeeeep <stephenedave.dorado@msugensan.edu.ph>
2024-02-17 19:03:32 +0800

    create dialog for add contribution

---
+++ Pages/Contribution/AddContribution.razor.cs
@@ -0,0 +1,55 @@
+﻿using BikeSparesInventorySystem.Shared.Dto;
+
+namespace BikeSparesInventorySystem.Pages.Contribution;
+
+public partial class AddContribution
+{
+    protected AddContributionDto _Contribution;
+
+    [CascadingParameter]
+    public MudDialogInstance MudDialog { get; set; } = default!;
+    [Parameter] public Action ChangeParentState { get; set; }
+
+    protected MudForm Form = default!;
+    protected bool IsSaving;
+    protected bool Disable;
+
+    public double ? Tithes;
+    public double ? Charity;
+    public double ? Car;
+    public double ? Others;
+    public Status Status;
+
+    protected async Task SubmitContribution()
+    {
+        IsSaving = true;
+        await Form.Validate();
+
+        if(Form.IsValid)
+        {
+            try
+            {
+                Contributions contributions = new()
+                {
+                    Tithes = Tithes,
+                    Charity = Charity,
+                    CarExpense = Car,
+                    Others = Others,
+                    Status = Status
+                };
+
+                ContributionRepository.Add(contributions);
+                await ContributionRepository.FlushAsync();
+                await Task.Delay(300);
+                ChangeParentState.Invoke();
+
+                Snackbar.Add($"Contribution is added successfully");
+                MudDialog.Close();
+            }
+            finally { IsSaving = false; }
+        }
+    }
+
+    protected void Cancel() => MudDialog.Cancel();
+
+}
